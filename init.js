eba05fefa2c66d22fcf84915adf987ee71cf4b03
shadowlillyrobotics <45044283+shadowlillyrobotics@users.noreply.github.com>
2018-11-14 09:52:57 -0800

    Update init.js

--- init.js
+++ init.js
@@ -52,28 +52,24 @@
 				{command: "soundcloudsearch [query]", description: "Search soundcloud for a song"}
 			]},
 			{category: "Reactions", description: "I react to your stupidity : D", commands: [
-				{command: "boop [person]", description: "Boop a Snoot! :3"},
 				{command: "bap [person]", description: "Bap someone! :french_bread:"},
 				{command: "shootlasers", description: "Pew pew pew"},
 				{command: "insult [person]", description: "Insult someone >:3"},
 				{command: "say [phrase]", description: "Have the bot say something... snekkily"},
 			]},
-			{category: "Currency", description: "Become the richest pony alive! In... ya know... a horribly inflated currency...", commands: [
+			{category: "Currency", description: "Become the richest person alive! In... ya know... a horribly inflated currency...", commands: [
 				{command: "money", description: "Check how many bits you have!"},
 				{command: "earn", description: "Earn some more bits! :3"},
 			]},
 			{category: "Images", description: "And YOU get an image, and YOU get an image!", commands: [
 				{command: "cat", description: "Gives a random cat image :3c"},
 				{command: "dog", description: "Gives a random dog image ▼・ᴥ・▼"},
-				{command: "derpi-top [query]", description: "Get the top-scored image for the search term. Will only return SFW images in non-NSFW channels"},
-				{command: "maid", description: "**NSFW channels only** >w> Use a different command, naughty butt"},
 				{command: "pfp [user]", description: "Get someone's profile picture, or your own! :3"},
 				{command: "greyscale [image url]", description: "Turn an image to greyscale"},
 				{command: "blur [image url] [intensity]", description: "Blur an image"}
 			]},	
 			{category: "Utilities", description: "Because sometimes, we actually have to do *useful* things", commands: [
 				{command: "emotes", description: "Displays all emotes accessable by the bot"},
-				{command: "patreon", description: "Displays my patreon account! :3"},
 				{command: "userconfig [option]", description: "Toggle personal optional settings\nThe two optional settings are *dad*, which alters the state of dad jokes, and *someone*, which alters the state of @someone pings"},
 				{command: "help [category/command (optional)]", description: "Displays information on a command or category. If none is specified, all categories are listed"},
 				{command: "ping", description: "Pings the bot"},
