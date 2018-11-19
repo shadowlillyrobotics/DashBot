fca881f27538bf3d2b0e99b4c0a895321d087a76
shadowlillyrobotics <45044283+shadowlillyrobotics@users.noreply.github.com>
2018-11-19 08:31:45 -0800

    Update init.js

--- init.js
+++ init.js
@@ -13,7 +13,7 @@
   			authTimeout: 5000,
  			tlsOptions: { rejectUnauthorized: false },
   			mailbox: "INBOX",
-  			searchFilter: ["FROM: " + process.env.email],
+  			searchFilter: [],
   			markSeen: false,
   			fetchUnreadOnStart: false,
   			attachments: false,
